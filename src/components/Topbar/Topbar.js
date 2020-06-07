99232789946fa4bb199f5eea1169da3ee016a802
Damian Wróblewski <dwroblewski89@gmail.com>
2020-06-07 21:43:50 +0200

    Add Layout structure

---
+++ src/components/Topbar/Topbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Topbar.module.css';
+
+const topbar = () => {
+    return (
+        <div>
+            topbar
+        </div>
+    );
+}
---
+++ src/hoc/Layout.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Topbar from '../components/Topbar.js';
+import classes from './Layout.module.css';
+
+
+const layout = ({ children }) => {
+    return (
+        <React.Fragment className={classes.Layout}>
+            <Topbar />
+            <div className={classes.Main}>{children}</div>
+        </React.Fragment>
+    );
+}
+
+export default layout;
