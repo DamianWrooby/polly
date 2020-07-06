37bfcd484ecf4f1dfc89ad4b7328759cca564019
Damian Wróblewski <dwroblewski89@gmail.com>
2020-07-06 22:55:55 +0200

    Add svg waves and watermark logo

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import Logo from '../Logo/Logo';
 import NavigationItems from '../Navigation/NavigationItems/NavigationItems';
-
 import classes from './Topbar.module.css';
 
 
 const Topbar = () => {
     return (
         <header className={classes.Topbar}>
-            <Logo />
+            <div className={classes.Watermark}>Polly</div>
             <nav>
                 <NavigationItems />
             </nav>
