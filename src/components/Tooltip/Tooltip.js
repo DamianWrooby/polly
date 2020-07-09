a8453bfd150d4e0169eabc7eb656f3d2afd7668a
Damian Wróblewski <dwroblewski89@gmail.com>
2020-07-09 22:42:06 +0200

    Add tooltip styles and animation

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import classes from './Tooltip.module.css';
 
 const Tooltip = ({ label, value, maxValue }) => {
     return (
-        <div className={classes.Tooltip}>
+        <div>
             <p>{label}</p>
             <p>{value}</p>
             <p>{maxValue}</p>
