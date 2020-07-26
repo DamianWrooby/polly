db47bda714a3d6b9775b3211bed1b48b366b0850
Damian Wróblewski <dwroblewski89@gmail.com>
2020-07-26 23:06:56 +0200

    Updates

---
+++ precache-manifest.84375490db0be85c0cbb77c5d3efde88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae9f554dbd2ff740daa350be2c98fb73",
+    "url": "/polly/index.html"
+  },
+  {
+    "revision": "c96fe960629f957d9967",
+    "url": "/polly/static/css/main.df90ff7c.chunk.css"
+  },
+  {
+    "revision": "8b2e736ee07ab9cd1b63",
+    "url": "/polly/static/js/2.0baf7412.chunk.js"
+  },
+  {
+    "revision": "23ec6c7f0411a6141095a6c739b32980",
+    "url": "/polly/static/js/2.0baf7412.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c96fe960629f957d9967",
+    "url": "/polly/static/js/main.79d69579.chunk.js"
+  },
+  {
+    "revision": "093d9458356abfa25912",
+    "url": "/polly/static/js/runtime-main.97113477.js"
+  },
+  {
+    "revision": "96a41a112b2210a9c9fa9f61ecf03645",
+    "url": "/polly/static/media/clouds-bg-c.96a41a11.jpg"
+  }
+]);
