0f052b202f8b447231f0d9671b5b56912e97206f
Damian Wróblewski <dwroblewski89@gmail.com>
2020-07-26 23:12:53 +0200

    Change router base path

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import About from './components/About/About';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/polly">
       <Layout>
         <Switch>
           <Route path="/" exact component={Front} />
