ec175bd1e197bb8e420744a1ee542af2e3177f7e
Damian Wróblewski <dwroblewski89@gmail.com>
2020-07-27 18:34:18 +0200

    Add credits and improve about page responsiveness

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -14,16 +14,23 @@ const About = () => {
       <h1>Polly</h1>
       <h2>Check air pollution in your area</h2>
       <p>
-        Polly is my first React project created from scratch. It allows users to
-        check air quality in their area. Air pollution is now considered to be
-        the world's largest environmental health threat, accounting for 7
-        million deaths around the world every year. Air pollution causes and
-        exacerbates a number of diseases, ranging from asthma to cancer,
-        pulmonary illnesses and heart disease. Especially in Poland, country
-        were I come from, air pollution is a big issue - we are one of the most
-        polluted country in Europe because of energy managment based on fossil
-        fuels.
+        Polly is a web application that allows users to check air quality in
+        their area. Air pollution is now considered to be the world's largest
+        environmental health threat, accounting for 7 million deaths around the
+        world every year. Air pollution causes and exacerbates a number of
+        diseases, ranging from asthma to cancer, pulmonary illnesses and heart
+        disease. Especially in Poland, country were I come from, air pollution
+        is a big issue - we are one of the most polluted country in Europe
+        because of energy managment based on fossil fuels.
       </p>
+      <div className={classes.Credits}>
+        <p>
+          Created by{' '}
+          <span>
+            <a href="https://twitter.com/DamianWrooby">Wrooby</a>
+          </span>
+        </p>
+      </div>
     </div>
   );
 };
