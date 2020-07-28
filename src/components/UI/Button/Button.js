ce63aaac64321846bf8736a39a0b31167f5cf117
Damian Wróblewski <dwroblewski89@gmail.com>
2020-07-28 22:36:05 +0200

    Add propTypes to button component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,9 +1,11 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import classes from './Button.module.css';
 
 const Button = ({ type, icon, disabled, clicked, ariaLabel }) => {
   return (
     <button
+      // eslint-disable-next-line react/button-has-type
       type={type}
       disabled={disabled}
       onClick={clicked}
@@ -11,11 +13,24 @@ const Button = ({ type, icon, disabled, clicked, ariaLabel }) => {
     >
       <div className={classes.Wrapper}>
         <div className={classes.Box}>
-          <i className={icon} aria-hidden="true"></i>
+          <i className={icon} aria-hidden="true" />
         </div>
       </div>
     </button>
   );
+};
+
+Button.defaultProps = {
+  disabled: false,
+  type: 'button',
+};
+
+Button.propTypes = {
+  ariaLabel: PropTypes.string.isRequired,
+  clicked: PropTypes.func.isRequired,
+  disabled: PropTypes.bool,
+  icon: PropTypes.string.isRequired,
+  type: PropTypes.string,
 };
 
 export default Button;
