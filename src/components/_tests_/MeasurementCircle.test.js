de370c32dbbba105b6b06c792beeceffb6335b86
Damian Wróblewski <dwroblewski89@gmail.com>
2020-09-05 13:00:05 +0200

    Remove debuging from test

--- src/components/_tests_/MeasurementCircle.test.js
+++ src/components/_tests_/MeasurementCircle.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import MeasurementCircle from '../MeasurementCircle/MeasurementCircle';
 
@@ -8,7 +8,6 @@ describe('MeasurementCircle', () => {
     const { getByText, getAllByText } = render(
       <MeasurementCircle label="Label" value={30} maxValue={40} />
     );
-    screen.debug();
     expect(getAllByText(/Label/)).toHaveLength(2);
     expect(getByText(/30/)).toBeInTheDocument();
     expect(getByText(/40/)).toBeInTheDocument();
